7ba732b03747e334e164673ef368722b2205389c
Srikanth0246 <tippasanisrikanth9966@gmail.com>
2022-07-26 12:04:44 +0530

    First

---
+++ srik.c
@@ -0,0 +1,62 @@
+/*#include<stdio.h>
+
+int main(){
+	int srik,i,mad;
+	printf("Enter the table we want:-");
+	scanf("%d",&srik);
+	printf("no of iterations:-");
+	scanf("%d",&mad);
+	for(i=1;i<=mad;i++){
+		printf("%d*%d=%d\n",srik,i,srik*i);
+	}
+return 0;
+}*/
+
+
+/*#include<stdio.h>
+int main(){
+	int siva,k,sad;
+	printf("enter the we want:-");
+	scanf("%d",&siva);
+    printf("no of intarations:-");
+	scanf("%d",&sad);
+	for(k=1;k<=sad;k++){
+		printf("%d*%d=%d\n",siva,k,siva*k);
+		
+	}
+return 0;
+}*/
+
+/*#include<stdio.h>
+
+int main(){
+	int a=0,b=1,c=0,n;
+	printf("Enter the range we want:-");
+	scanf("%d",&n);
+	for(int i=0;i<=n;i++){
+		a=b;
+		b=c;
+		c=a+b;
+	printf("%d\n",c);
+	}
+    
+return 0;
+}
+*/
+#include<stdio.h>
+int main(){
+	int a=0,b=8,c=0,n;
+	printf("enter the range we want:-");
+	scanf("%d",&n);
+	for(int i=0;i<n;i++){
+		a=b;
+		b=c;
+		c=a+b;
+		printf("%d\n",c);
+		
+		
+	}
+
+return 0;
+
+}
